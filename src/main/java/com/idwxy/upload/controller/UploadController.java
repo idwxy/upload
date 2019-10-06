e0e3868a1b6db1a3da58d6bed5a332f2514f1d19
idwxy <idwxy@live.com>
2019-10-06 17:32:41 +0800

    上传文件控制器

---
+++ src/main/java/com/idwxy/upload/controller/UploadController.java
@@ -0,0 +1,47 @@
+package com.idwxy.upload.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+@RestController
+public class UploadController {
+
+    // 文件保存路径
+    private final static String FILE_UPLOAD_PATH = "D:/upload/";
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file")MultipartFile file) {
+        // 文件为空返回失败
+        if (file.isEmpty()) {
+            return "上传失败";
+        }
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        // 生成文件名称通用方法
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Random r = new Random();
+        StringBuilder tempName = new StringBuilder();
+        tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
+        String newFileName = tempName.toString();
+        try {
+            // 保存文件
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(FILE_UPLOAD_PATH + newFileName);
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传成功";
+    }
+}
